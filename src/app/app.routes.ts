bbe9262005bc3b7e06e392e89fb72e866d08745f
a_masalevich <a.masalevich@belitsoft.com>
2023-11-10 19:21:22 +0100

    added game-page component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {GamePageComponent} from "./pages/game-page/game-page.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'',
+    component:GamePageComponent,
+  }
+];
---
+++ src/app/pages/game-page/game-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-game-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './game-page.component.html',
+  styleUrl: './game-page.component.scss'
+})
+export class GamePageComponent {
+
+}
