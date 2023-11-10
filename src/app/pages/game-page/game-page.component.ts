7ac8dbaa9162535730446b552e2196a5db4fa263
a_masalevich <a.masalevich@belitsoft.com>
2023-11-10 20:07:25 +0100

    added textures, animation etc

--- src/app/pages/game-page/game-page.component.ts
+++ src/app/pages/game-page/game-page.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {AfterViewChecked, Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import * as PIXI from 'pixi.js'
+
 
 @Component({
   selector: 'app-game-page',
@@ -8,6 +10,52 @@ import { CommonModule } from '@angular/common';
   templateUrl: './game-page.component.html',
   styleUrl: './game-page.component.scss'
 })
+
 export class GamePageComponent {
+
+  app!: PIXI.Application<HTMLCanvasElement>;
+
+  ticker!: PIXI.Ticker;
+
+  groundTexture!: PIXI.Texture;
+
+  skyTexture!: PIXI.Texture;
+
+  groundTilingSprite!: PIXI.TilingSprite;
+
+  skyTilingSprite!: PIXI.TilingSprite;
+
+  constructor() {
+    this.init()
+  }
+
+  init() {
+    this.app = new PIXI.Application<HTMLCanvasElement>({width: 800, height: 600})
+    document.body.appendChild(this.app.view)
+    this.ticker = PIXI.Ticker.shared;
+    this.createTextures()
+  }
+
+  createTextures() {
+    this.groundTexture = PIXI.Texture.from("../../../assets/ground.png");
+    this.skyTexture = PIXI.Texture.from("../../../assets/sky.png");
+    this.groundTilingSprite = new PIXI.TilingSprite(
+      this.groundTexture, this.app.screen.width, this.app.screen.height
+    )
+    this.skyTilingSprite = new PIXI.TilingSprite(
+      this.skyTexture, this.app.screen.width, this.app.screen.height
+    )
+    this.app.stage.addChild(this.skyTilingSprite, this.groundTilingSprite)
+    console.log(this.skyTilingSprite.tilePosition)
+    this.ticker.add(() => {
+      this.groundTilingSprite.tilePosition.x -= 1;
+      this.skyTilingSprite.tilePosition.x -= 0.2;
+    })
+  }
+
+  animateEnvironment() {
+    this.groundTilingSprite.tilePosition.x -= 1;
+    this.skyTilingSprite.tilePosition.x -= 0.2;
+  }
 
 }
