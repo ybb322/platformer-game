dcbc7623617b51459243ee4ec60c43b5b66b5d47
a_masalevich <a.masalevich@belitsoft.com>
2023-11-12 23:35:53 +0100

    replaced person with corgi; added ticker.remove()

--- src/app/pages/game-page/game-page.component.ts
+++ src/app/pages/game-page/game-page.component.ts
@@ -31,6 +31,10 @@ export class GamePageComponent {
 
   landTween!: TWEEN.Tween<PIXI.ObservablePoint<any>>
 
+  corgi!: PIXI.Sprite;
+
+  corgiRunningTexture!: PIXI.Texture;
+
   constructor() {
     this.init()
   }
@@ -40,7 +44,7 @@ export class GamePageComponent {
     document.body.appendChild(this.app.view)
     this.ticker = PIXI.Ticker.shared;
     this.handleTextures()
-    this.handlePerson()
+    this.handleCorgi()
   }
 
   handleTextures() {
@@ -66,21 +70,69 @@ export class GamePageComponent {
     this.app.stage.addChild(this.person)
     this.person.y = 75;
     this.person.x = -200;
-    this.jumpTween = new TWEEN.Tween(this.person.position).to({y: -20}, 350).easing(TWEEN.Easing.Quadratic.Out);
-    this.landTween = new TWEEN.Tween(this.person.position).to({y: 75}, 350).easing(TWEEN.Easing.Quadratic.In);
-    this.jumpTween.chain(this.landTween);
+    // this.jumpTween = new TWEEN.Tween(this.person.position).to({y: -20}, 350).easing(TWEEN.Easing.Quadratic.Out);
+    // this.landTween = new TWEEN.Tween(this.person.position).to({y: 75}, 350).easing(TWEEN.Easing.Quadratic.In);
+    // this.jumpTween.chain(this.landTween);
+
+    const jumping = () => {
+      console.log('jumping!')
+      this.jumpTween.update()
+      this.landTween.update()
+      this.landTween.onComplete(() => {
+        console.log('removed!')
+        this.ticker.remove(jumping)
+      })
+    }
+
     document.addEventListener('keyup', event => {
       if (event.code === 'Space') {
-        this.jump()
+        this.jumpTween.start()
+        this.ticker.add(jumping)
       }
     })
   }
 
-  jump() {
-    this.jumpTween.start()
-    this.ticker.add(() => {
+  handleCorgi() {
+    const corgiRunningTextureOne = PIXI.Texture.from('../../../assets/corgi_running_1.png')
+    const corgiRunningTextureTwo = PIXI.Texture.from('../../../assets/corgi_running_2.png')
+    this.corgiRunningTexture = corgiRunningTextureOne;
+    this.corgi = PIXI.Sprite.from(this.corgiRunningTexture)
+    this.app.stage.addChild(this.corgi)
+    this.corgi.y = 400;
+    this.corgi.x = 50;
+
+    this.jumpTween = new TWEEN.Tween(this.corgi.position).to({y: 320}, 350).easing(TWEEN.Easing.Quadratic.Out);
+    this.landTween = new TWEEN.Tween(this.corgi.position).to({y: 400}, 350).easing(TWEEN.Easing.Quadratic.In);
+    this.jumpTween.chain(this.landTween);
+
+    const jumping = () => {
+      console.log('jumping!')
       this.jumpTween.update()
       this.landTween.update()
+      this.landTween.onComplete(() => {
+        console.log('removed!')
+        this.ticker.remove(jumping)
+      })
+    }
+
+    document.addEventListener('keyup', event => {
+      if (event.code === 'Space') {
+        this.jumpTween.start()
+        this.ticker.add(jumping)
+      }
     })
+
+
+    setInterval(() => {
+      console.log(this.corgiRunningTexture.textureCacheIds)
+      if (this.corgi.texture === corgiRunningTextureOne) {
+        this.corgi.texture = corgiRunningTextureTwo
+      } else {
+        this.corgi.texture = corgiRunningTextureOne
+      }
+    }, 150)
+
+
   }
+
 }
